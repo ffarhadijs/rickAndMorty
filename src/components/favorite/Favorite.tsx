a97a921be214311683f6765e4f8f03d1c542f20f
farhad <rb0214192@gmail.com>
2022-12-16 16:29:42 +0330

    base style added

--- src/components/favorite/Favorite.tsx
+++ src/components/favorite/Favorite.tsx
@@ -1,6 +1,29 @@
+import { Box, Typography } from "@mui/material";
+
 const Favorite = () => {
-    return(
-        <div>favorite</div>
-    )
+  return (
+    <Box
+      bgcolor={"#CCD1D9"}
+      width={"300px"}
+      minHeight={"100vh"}
+      p={"20px"}
+      display={"flex"}
+      flexDirection={"column"}
+      rowGap={"30px"}
+    >
+      <Typography
+        sx={{
+          fontSize: "30px",
+          textAlign: "center",
+          pb: "20px",
+          color: "#434A54",
+          borderBottom: "1px solid gray",
+          fontWeight: "900",
+        }}
+      >
+        Favorites
+      </Typography>
+    </Box>
+  );
 };
 export default Favorite;
