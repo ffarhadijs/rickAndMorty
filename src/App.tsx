341eb7c7c0c163443dcb7e7b206cf10370d4691c
farhad <rb0214192@gmail.com>
2022-12-21 10:14:22 +0330

    updated with context and queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import { QueryClientProvider, QueryClient } from "react-query";
+import ProductDetails from "./pages/productDetails/ProductDetails";
+import FavoriteContextProvider from "./contexts/FavoriteContext";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <Home />
+      <FavoriteContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/:id" element={<ProductDetails />} />
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </FavoriteContextProvider>
     </>
   );
 }
