4c3f1473ca5c7117f748cd2dc62778bf3188efca
farhad <rb0214192@gmail.com>
2022-12-21 10:10:08 +0330

    pagination created

--- src/components/productsList/ProductsList.tsx
+++ src/components/productsList/ProductsList.tsx
@@ -1,6 +1,35 @@
-const ProductList = () => {
+import { Grid, Box, Pagination } from "@mui/material";
+import Product from "../product/Product";
+
+type ProductsListProps = {
+  data: any;
+  setPageNum: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const ProductList = (props: ProductsListProps) => {
   return (
-    <div>list</div>
+    <Box>
+      <Grid container spacing={3} p={3}>
+        {props.data?.data.results.map((item: any) => (
+          <Grid item sm={12} md={4} lg={3} key={item.id}>
+            <Product item={item} />
+          </Grid>
+        ))}
+      </Grid>
+      <Box display={"flex"} flexDirection={"row"} justifyContent="center">
+        <Pagination
+          size="large"
+          sx={{
+            display: "inline-block",
+            padding: "20px 0px",
+          }}
+          count={props.data?.data.info.pages}
+          onChange={(event: React.ChangeEvent<unknown>, page: number) =>
+            props.setPageNum(page)
+          }
+        />
+      </Box>
+    </Box>
   );
 };
 export default ProductList;
