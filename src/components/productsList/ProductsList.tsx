8540190427ab493a6c0aab744137d1e1e8852c40
farhad <rb0214192@gmail.com>
2023-03-21 17:31:06 +0330

    update

--- src/components/productsList/ProductsList.tsx
+++ src/components/productsList/ProductsList.tsx
@@ -1,21 +1,35 @@
 import { Grid, Box } from "@mui/material";
+import { FC } from "react";
 import Product from "../product/Product";
 
-type ProductsListProps = {
-  data: any;
-};
+interface IProductsListProps {
+  data: {
+    results: [IProductProps];
+  };
+}
 
-const ProductList = (props: ProductsListProps) => {
+interface IProps {
+  data: IProductsListProps;
+}
+
+export interface IProductProps {
+  id: number;
+  image: string;
+  name: string;
+  gender: string;
+  status: string;
+}
+
+const ProductList: FC<IProps> = ({ data }) => {
   return (
     <Box>
       <Grid container spacing={3} p={3}>
-        {props.data?.data.results.map((item: any) => (
-          <Grid item sm={12} md={4} lg={3} key={item.id}>
+        {data.data.results.map((item) => (
+          <Grid item xs={12} sm={4} md={3} lg={2} key={item.id}>
             <Product item={item} />
           </Grid>
         ))}
       </Grid>
-      
     </Box>
   );
 };
