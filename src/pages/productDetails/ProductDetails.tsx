a81383ec9c9b0677400894574f7679714fead11d
farhad <rb0214192@gmail.com>
2023-03-21 17:32:41 +0330

    update

--- src/pages/productDetails/ProductDetails.tsx
+++ src/pages/productDetails/ProductDetails.tsx
@@ -6,6 +6,7 @@ import Favorite from "../../components/favorite/Favorite";
 import { useContext } from "react";
 import { FavoriteContext } from "../../contexts/FavoriteContext";
 import ProductDetail from "../../components/productDetail/ProductDetail";
+import Box from "@mui/material/Box";
 
 const ProductDetails = () => {
   const { state } = useContext(FavoriteContext);
@@ -21,52 +22,19 @@ const ProductDetails = () => {
   const { data, error, status } = useQuery(["getProduct", id], getProduct);
 
   return (
-    <Grid container>
-      <Grid
-        item
-        xs={3}
-        sx={{
-          backgroundColor: "#CCD1D9",
-          p: "20px",
-          height: "100%",
-          minHeight: "100vh",
-        }}
-      >
-        <Typography
-          sx={{
-            fontSize: "30px",
-            textAlign: "center",
-            pb: "20px",
-            color: "#434A54",
-            borderBottom: "1px solid gray",
-            fontWeight: "900",
-          }}
-        >
-          Rick and Morty
+    <Box marginX={"auto"} padding="20px">
+      {status === "error" && error instanceof Error && (
+        <Typography variant={"body1"} textAlign="center">
+          {error.message}
         </Typography>
-      </Grid>
-      <Grid item p={4} xs={6}>
-        {status === "error" && error instanceof Error && (
-          <Typography variant={"body1"} textAlign="center">
-            {error.message}
-          </Typography>
-        )}
-        {status === "success" && <ProductDetail data={data} />}
-        {status === "loading" && (
-          <Typography variant="body1" textAlign="center">
-            loading...
-          </Typography>
-        )}
-      </Grid>
-      <Grid
-        item
-        xs={3}
-        minHeight={"100vh"}
-        display={state.favoriteList.length > 0 ? "block" : "none"}
-      >
-        <Favorite />
-      </Grid>
-    </Grid>
+      )}
+      {status === "success" && <ProductDetail data={data} />}
+      {status === "loading" && (
+        <Typography variant="body1" textAlign="center">
+          loading...
+        </Typography>
+      )}
+    </Box>
   );
 };
 
