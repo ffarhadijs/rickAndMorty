bdd433e07d6ceecbae9cb0ae01267b7e67a561c3
farhad <rb0214192@gmail.com>
2023-03-21 17:30:43 +0330

    make more responsive

--- src/components/productDetail/ProductDetail.tsx
+++ src/components/productDetail/ProductDetail.tsx
@@ -1,73 +1,94 @@
-import { Stack, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
+import { FC } from "react";
 
-type ProductDetailType = {
-  data: any;
-};
-const ProductDetail = (props: ProductDetailType) => {
+interface ProductDetailType {
+  data: {
+    data: {
+      image: string;
+      name: string;
+      gender: string;
+      status: string;
+      species: string;
+      location: {
+        name: string;
+      };
+    };
+  };
+}
+const ProductDetail: FC<ProductDetailType> = ({ data }) => {
   return (
-    <Stack
-      flexDirection={"row"}
+    <Grid
+      container
       sx={{
-        height: "200px",
-        width: "auto",
+        marginX:"auto",
+        width: { xs: "270px", sm: "max-content" },
+        height: "auto",
         backgroundColor: "#F5F7FA",
       }}
     >
-      <img src={props.data.data.image} alt={props.data.data.name} />
-      <Stack flexDirection={"column"} p={2} rowGap={1}>
-        <Typography variant="h5">{props.data.data.name}</Typography>
-        <Typography
-          variant="body1"
-          sx={{
-            color:
-              props.data.data.status === "Dead"
-                ? "red"
-                : props.data.data.status === "Alive"
-                ? "green"
-                : "black",
-          }}
-        >
+      <Grid item xs={12} sm={4}>
+        <img
+          src={data.data.image}
+          alt={data.data.name}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </Grid>
+      <Grid item xs={12} sm={8} sx={{ backgroundColor: "" }}>
+        <Stack flexDirection={"column"} p={2} rowGap={1}>
+          <Typography variant="h6">{data.data.name}</Typography>
           <Typography
             variant="body1"
-            component={"span"}
-            sx={{ fontWeight: "bold", color: "black" }}
+            sx={{
+              color:
+                data.data.status === "Dead"
+                  ? "red"
+                  : data.data.status === "Alive"
+                  ? "green"
+                  : "black",
+            }}
           >
-            status:
-          </Typography>{" "}
-          {props.data.data.status}
-        </Typography>
-        <Typography variant="body1">
-          <Typography
-            variant="body1"
-            component={"span"}
-            sx={{ fontWeight: "bold", color: "black" }}
-          >
-            gender:
-          </Typography>{" "}
-          {props.data.data.gender}
-        </Typography>
-        <Typography variant="body1">
-          <Typography
-            variant="body1"
-            component={"span"}
-            sx={{ fontWeight: "bold", color: "black" }}
-          >
-            location:
-          </Typography>{" "}
-          {props.data.data.location.name}
-        </Typography>
-        <Typography variant="body1">
-          <Typography
-            variant="body1"
-            component={"span"}
-            sx={{ fontWeight: "bold", color: "black" }}
-          >
-            species:
-          </Typography>{" "}
-          {props.data.data.species}
-        </Typography>
-      </Stack>
-    </Stack>
+            <Typography
+              variant="body1"
+              component={"span"}
+              sx={{ fontWeight: "bold", color: "black" }}
+            >
+              status:
+            </Typography>{" "}
+            {data.data.status}
+          </Typography>
+          <Typography variant="body1">
+            <Typography
+              variant="body1"
+              component={"span"}
+              sx={{ fontWeight: "bold", color: "black" }}
+            >
+              gender:
+            </Typography>{" "}
+            {data.data.gender}
+          </Typography>
+          <Typography variant="body1">
+            <Typography
+              variant="body1"
+              component={"span"}
+              sx={{ fontWeight: "bold", color: "black" }}
+            >
+              location:
+            </Typography>{" "}
+            {data.data.location.name}
+          </Typography>
+          <Typography variant="body1">
+            <Typography
+              variant="body1"
+              component={"span"}
+              sx={{ fontWeight: "bold", color: "black" }}
+            >
+              species:
+            </Typography>{" "}
+            {data.data.species}
+          </Typography>
+        </Stack>
+      </Grid>
+    </Grid>
   );
 };
 
