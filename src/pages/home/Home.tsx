9dc6a9af0cfa2fa34b114c443aefe3089ee28e7e
farhad <rb0214192@gmail.com>
2023-03-21 17:32:18 +0330

    update

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,16 @@
 import { Grid, Typography, Pagination, Box } from "@mui/material";
 import axios from "axios";
-import { useState, useContext } from "react";
+import { useState, useContext, ChangeEvent, FC } from "react";
 import { useQuery } from "react-query";
 import { useSearchParams } from "react-router-dom";
 import Favorite from "../../components/favorite/Favorite";
 import Filter from "../../components/filter/Filter";
 import ProductList from "../../components/productsList/ProductsList";
 import { FavoriteContext } from "../../contexts/FavoriteContext";
+import { SearchParams } from "../../contexts/SearchParamsContextProvider";
 
-const Home = () => {
-  const { state } = useContext(FavoriteContext);
-  const [searchParams, setSearchParams] = useSearchParams();
+const Home: FC = () => {
+  const { searchParams } = useContext(SearchParams);
   const [pageNum, setPageNum] = useState(1);
   const getApi = (params: { queryKey: any }) => {
     return axios.get(
@@ -35,51 +35,39 @@ const Home = () => {
     ],
     getApi
   );
+
   return (
-    <Grid container>
-      <Grid item xs={2.5} minHeight={"100vh"}>
-        <Filter searchParams={searchParams} setSearchParams={setSearchParams} />
-      </Grid>
-      <Grid item xs={state.favoriteList.length > 0 ? 7 : 9.5}>
-        {status === "success" && (
-          <>
-            <ProductList data={data} />
-            <Box display={"flex"} flexDirection={"row"} justifyContent="center">
-              <Pagination
-                size="large"
-                sx={{
-                  display: "inline-block",
-                  padding: "20px 0px",
-                }}
-                page={pageNum}
-                count={data?.data.info.pages}
-                onChange={(event: React.ChangeEvent<unknown>, page: number) =>
-                  setPageNum(page)
-                }
-              />
-            </Box>
-          </>
-        )}
-        {status === "loading" && (
-          <Typography variant="body1" textAlign={"center"}>
-            ...loading
-          </Typography>
-        )}
-        {status === "error" && error instanceof Error && (
-          <Typography variant="body1" textAlign={"center"}>
-            {error.message}
-          </Typography>
-        )}
-      </Grid>
-      <Grid
-        item
-        xs={2.5}
-        m={0}
-        display={state.favoriteList.length > 0 ? "block" : "none"}
-      >
-        <Favorite />
-      </Grid>
-    </Grid>
+    <Box>
+      {status === "success" && (
+        <>
+          <ProductList data={data} />
+          <Box display={"flex"} flexDirection={"row"} justifyContent="center">
+            <Pagination
+              size="large"
+              sx={{
+                display: "inline-block",
+                padding: "20px 0px",
+              }}
+              page={pageNum}
+              count={data?.data.info.pages}
+              onChange={(e: ChangeEvent<unknown>, page: number) =>
+                setPageNum(page)
+              }
+            />
+          </Box>
+        </>
+      )}
+      {status === "loading" && (
+        <Typography variant="body1" textAlign={"center"}>
+          ...loading
+        </Typography>
+      )}
+      {status === "error" && error instanceof Error && (
+        <Typography variant="body1" textAlign={"center"}>
+          {error.message}
+        </Typography>
+      )}
+    </Box>
   );
 };
 
