3f37120e4753d6926284ae253f65f68d68d99a41
farhad <rb0214192@gmail.com>
2023-03-21 17:33:00 +0330

    add search params context

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,27 @@ import Home from "./pages/home/Home";
 import { QueryClientProvider, QueryClient } from "react-query";
 import ProductDetails from "./pages/productDetails/ProductDetails";
 import FavoriteContextProvider from "./contexts/FavoriteContext";
+import NavBar from "./components/navBar/NavBar";
+import SearchParamsContextProvider from "./contexts/SearchParamsContextProvider";
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: { refetchOnWindowFocus: false },
+    },
+  });
   return (
     <>
       <FavoriteContextProvider>
         <QueryClientProvider client={queryClient}>
           <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/:id" element={<ProductDetails />} />
-            </Routes>
+            <SearchParamsContextProvider>
+              <NavBar />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:id" element={<ProductDetails />} />
+              </Routes>
+            </SearchParamsContextProvider>
           </BrowserRouter>
         </QueryClientProvider>
       </FavoriteContextProvider>
